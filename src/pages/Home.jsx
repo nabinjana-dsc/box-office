fd928c35810bb0ddcab7f47624955fd4b3d3ac6d
nabinjana255 <nabinjana7@gmail.com>
2024-01-06 17:56:40 +0530

    fixed prettier config mispell, add format script to package.json

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,14 +4,13 @@ import { searchForShows } from './../api/tvmaza';
 const Home = () => {
   const [searchStr, setSearchStr] = useState('');
   const [apiData, setApiData] = useState(null);
-  const [apiDataError, setApiDataError] = useState(null)
+  const [apiDataError, setApiDataError] = useState(null);
 
-
-  const onSearchInputChange = (ev) => {
+  const onSearchInputChange = ev => {
     setSearchStr(ev.target.value);
   };
 
-  const onSearch = async (ev) => {
+  const onSearch = async ev => {
     ev.preventDefault();
 
     try {
@@ -30,13 +29,13 @@ const Home = () => {
     }
 
     if (apiData) {
-      return apiData.map((data) => (
+      return apiData.map(data => (
         <div key={data.show.id}>{data.show.name}</div>
-        ));
+      ));
     }
 
     return null;
-  }
+  };
 
   return (
     <div>
