a479737e657b2589d5895fa84cdf1ae8f71d8a56
nabinjana255 <nabinjana7@gmail.com>
2024-01-07 00:49:59 +0530

    fetch data inside Show component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,27 @@
 import { useState } from 'react';
 import { searchForShows, searchForPeople } from './../api/tvmaza';
+import SearchForm from '../components/SearchForm';
+import ShowGrid from '../components/shows/ShowGrid';
+import ActorsGrid from '../components/actors/ActorsGrid';
+// import MainPageLayout from '../components/MainPageLayout';
 
 const Home = () => {
-  const [searchStr, setSearchStr] = useState('');
   const [apiData, setApiData] = useState(null);
   const [apiDataError, setApiDataError] = useState(null);
-  const [searchOption, setSearchOption] = useState('shows');
 
-  const onSearchInputChange = ev => {
-    setSearchStr(ev.target.value);
-  };
-
-  const onRadioChange = ev => {
-    setSearchOption(ev.target.value);
-  };
-
-  const onSearch = async ev => {
-    ev.preventDefault();
-
+  const onSearch = async ({ q, searchOption }) => {
     try {
       setApiDataError(null);
 
+      let result;
+
       if (searchOption === 'shows') {
-        const result = await searchForShows(searchStr);
-        setApiData(result);
+        result = await searchForShows(q);
       } else {
-        const result = await searchForPeople(searchStr);
-        setApiData(result);
+        result = await searchForPeople(q);
       }
+
+      setApiData(result);
     } catch (error) {
       setApiDataError(error);
     }
@@ -38,12 +32,12 @@ const Home = () => {
       return <div>Error occurred: {apiDataError.message}</div>;
     }
 
+    if (apiData?.length === 0) {
+      return <div>No results</div>
+    }
+
     if (apiData) {
-      return apiData[0].show
-        ? apiData.map(data => <div key={data.show.id}>{data.show.name}</div>)
-        : apiData.map(data => (
-            <div key={data.person.id}>{data.person.name}</div>
-          ));
+      return apiData[0].show ? (<ShowGrid shows ={apiData} />) : (<ActorsGrid actors={apiData} />);
     }
 
     return null;
@@ -51,33 +45,7 @@ const Home = () => {
 
   return (
     <div>
-      <form onSubmit={onSearch}>
-        <input type="text" value={searchStr} onChange={onSearchInputChange} />
-
-        <label>
-          Shows
-          <input
-            type="radio"
-            name="search-option"
-            value="shows"
-            checked={searchOption === 'shows'}
-            onChange={onRadioChange}
-          />
-        </label>
-
-        <label>
-          Actors
-          <input
-            type="radio"
-            name="search-option"
-            value="actors"
-            checked={searchOption === 'actors'}
-            onChange={onRadioChange}
-          />
-        </label>
-
-        <button type="submit">Search</button>
-      </form>
+      <SearchForm onSearch={onSearch} />
 
       <div>{renderApiData()}</div>
     </div>
@@ -85,3 +53,9 @@ const Home = () => {
 };
 
 export default Home;
+
+// apiData.map(data => <div key={data.show.id}>{data.show.name}</div>)
+
+// apiData.map(data => (
+//   <div key={data.person.id}>{data.person.name}</div>
+// ));
